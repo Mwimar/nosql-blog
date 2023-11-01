15990e5f58915ae5772708ffe32c41282f6b1b4e
Mark Mwima <markmwimar@gmail.com>
2023-11-01 18:49:25 +0300

    writing data to dbase

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
+const mongodb = require("mongodb");
+
 const db = require("../data/database");
+
+const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
@@ -14,12 +18,30 @@ router.get("/posts", function (req, res) {
 
 router.get("/new-posts", async function (req, res) {
   const authors = await db.getDb().collection("authors").find().toArray();
-  console.log(authors);
+  // console.log(authors);
   res.render("create-posts", { authors: authors });
 });
-// router.post("/posts", async function (req, res) {
-//   res.redirect("/posts");
-// });
+router.post("/posts", async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const author = await db
+    .getDb()
+    .collection("authors")
+    .findOne({ _id: authorId });
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+  const result = await db.getDb().collection("posts").insertOne(newPost);
+  console.log(result);
+  res.redirect("/posts");
+});
 
 // router.get("/posts/:id", function (req, res) {
 //   res.render("post-detail");
